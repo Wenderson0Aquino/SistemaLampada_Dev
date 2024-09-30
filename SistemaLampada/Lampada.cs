6ca3bc44f7c75c6aa11d4e410028f7681c141201
Wenderson Aquino <wensantos.aquino@gmail.com>
2024-09-30 20:47:41 -0300

    Add files via upload

---
+++ SistemaLampada/Lampada.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLampada
+{
+    internal class Lampada
+    {
+        bool ligada;
+        string voltagem;
+        string potencia;
+        Random random = new Random();
+
+        public Lampada(string voltagem, string potencia)
+        {
+            ligada = false;
+            this.voltagem = voltagem;
+            this.potencia = potencia;
+        }
+
+        public string Ligar()
+        {
+            ligada = true;
+            return "Lâmpada Ligada!";
+        }
+
+        public string Desligar()
+        {
+            ligada = false;
+            return "Lâmpada Desligada!";
+        }
+
+        public string LerPotencia()
+        {
+            return potencia;
+        }
+
+        public string LerVoltagem()
+        {
+            return voltagem;
+        }
+
+        public void AlterarPotencia(string potencia)
+        {
+            this.potencia = potencia;
+            Console.WriteLine(this.potencia);
+        }
+
+        public void AlterarVoltagem(string voltagem)
+        {
+            this.voltagem = voltagem;
+            Console.WriteLine(this.voltagem);
+        }
+
+        public void QueimarLampada()
+        {
+            if (ligada && random.NextDouble() == 0.15)
+            {
+                Console.WriteLine("A Lâmpada Queimou!");
+            }
+        }
+    }
+}
---
+++ SistemaLampada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLampada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampada lampada = new Lampada("127", "60");
+
+            Console.WriteLine(lampada.Ligar());
+            lampada.AlterarPotencia("220");
+            lampada.AlterarVoltagem("90");
+            lampada.QueimarLampada();
+
+            Console.ReadKey();
+        }
+    }
+}
